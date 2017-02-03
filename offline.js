0170ab3ecb387b901ed4c30b87eef0d9db3dddb7
egor009 <egor009@users.noreply.github.com>
2017-02-03 10:25:40 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1486099114,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/player-sheet0.png",
+		"images/9патч.png",
+		"images/Спрайт-sheet0.png",
+		"images/Спрайт2-sheet0.png",
+		"images/gun-sheet0.png",
+		"images/Спрайт3-sheet0.png",
+		"images/snarad-sheet0.png",
+		"images/Спрайт5-sheet0.png",
+		"images/zona-sheet0.png",
+		"images/oskolok-sheet0.png",
+		"images/ter-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"xd_connection.js"
+	]
+}
